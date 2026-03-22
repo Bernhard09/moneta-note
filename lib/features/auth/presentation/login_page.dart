a675851600e32c5f7f84b6b8ba49943c9100d5f0
Bernhard09 <bryantfawwaz1@gmail.com>
2026-03-22 23:49:15 +0700

    add login page v1

---
+++ lib/features/auth/presentation/login_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../../notes/presentation/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isObscured = false;
+
+  void _revealPassword() {
+    setState(() {
+      _isObscured = !_isObscured;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.widthOf(context);
+    final screenHeight = MediaQuery.heightOf(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(
+            horizontal: screenWidth * 0.05,
+            vertical: screenHeight * 0.1,
+          ),
+          child: Column(
+            spacing: 24.0,
+            children: [
+              Text('Login'),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'youremail@email.com',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextField(
+                obscureText: _isObscured,
+                decoration: InputDecoration(
+                  // contentPadding: EdgeInsets.symmetric(
+                  //   vertical: 8,
+                  //   horizontal: 10,
+                  // ),
+                  labelText: 'Password',
+                  hintText: 'password',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  suffix: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: Icon(
+                      _isObscured ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: _revealPassword,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (context) =>
+                                MyHomePage(title: 'Home Page'),
+                          ),
+                        );
+                      },
+                      child: Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
